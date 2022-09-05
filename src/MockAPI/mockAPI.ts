956cb69b74ed8ee8485c4fa0d16ad260b1ba0348
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-09-06 01:39:58 +0300

    mock API fetcher

---
+++ src/MockAPI/mockAPI.ts
@@ -0,0 +1,25 @@
+const CACHED_API_RESULTS_URL = '/cached-api-results.json';
+
+// Yeah JSON returns 'any', that's not just a lazy TypeScript
+const deepJSONCopy = (obj: any): any => JSON.parse(JSON.stringify(obj));
+
+let results: any;
+let resolveResultsAvailable: (value: unknown) => void;
+const resultsAvailable: Promise<any> = new Promise(resolve => {
+    resolveResultsAvailable = resolve;
+});
+
+(async function() {
+    const cachedAPIResults = await fetch(CACHED_API_RESULTS_URL);
+    results = await cachedAPIResults.json();
+    resolveResultsAvailable(results);
+})
+
+export async function getResult(key: string) {
+    if (results) {
+        return deepJSONCopy(results[key] || '');
+    } else {
+        const results = await resultsAvailable;
+        return deepJSONCopy(results[key] || '');
+    }
+}
