e0f09f66b0551d673490c629e549fec5d12683a1
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-09-06 13:52:26 +0300

    this should have been part of the previous commit

--- src/components/CitySearchBox.tsx
+++ src/components/CitySearchBox.tsx
@@ -1,34 +1,46 @@
 import { useEffect, useState } from "react";
-import { getAutocompleteList } from "../API/mockAPI";
+// import { getAutocompleteList } from "../API/mockAPI";
+import type { City } from "../API/autocomplete";
+import autocomplete from '../API/autocomplete';
 
-function fromAPIListToOptionList(list: any[]) {
-    let prevHasSameShortName = false;
+// function fromAPIListToOptionList(list: any[]) {
+//     let prevHasSameShortName = false;
 
-    return list.map((item, index) => {
-        const cityShortName = item.LocalizedName,
-            nextShortName = list[index + 1]?.LocalizedName;
+//     return list.map((item, index) => {
+//         const cityShortName = item.LocalizedName,
+//             nextShortName = list[index + 1]?.LocalizedName;
 
-        let currentlyNeedLongName = prevHasSameShortName;
-        prevHasSameShortName = cityShortName === nextShortName;
-        currentlyNeedLongName ||= prevHasSameShortName;
+//         let currentlyNeedLongName = prevHasSameShortName;
+//         prevHasSameShortName = cityShortName === nextShortName;
+//         currentlyNeedLongName ||= prevHasSameShortName;
                 
-        const cityName = currentlyNeedLongName ?
-            `${item.LocalizedName} (${item.Country.LocalizedName})` :
-            item.LocalizedName;
+//         const cityName = currentlyNeedLongName ?
+//             `${item.LocalizedName} (${item.Country.LocalizedName})` :
+//             item.LocalizedName;
 
-        return <option value={item.Key}>{cityName}</option>
-    });
+//         return <option value={item.Key}>{cityName}</option>
+//     });
+// }
+
+function citiesToOptionList(cities: City[]) {
+    return cities.map(city =>
+        <option
+            value={city.apiKey}
+            key={city.apiKey}
+        >
+            {city.name}
+        </option>
+    );
 }
 
 export default function CitySearchBox() {
     const [cityName, setCityName] = useState('Tel Aviv'),
         [cityKey, setCityKey] = useState(''),
-        [citiesList, setCitiesList] = useState<any[]>([]);
+        [citiesList, setCitiesList] = useState<City[]>([]);
 
     useEffect(() => {
-        console.log('effect');
         cityName && (async () => {
-            setCitiesList(await getAutocompleteList(cityName));
+            setCitiesList(await autocomplete(cityName));
         })();
     }, [cityName]);
 
@@ -48,12 +60,12 @@ export default function CitySearchBox() {
                 const newCityKey = e.currentTarget.value; 
                 setCityKey(newCityKey);
                 setCityName(
-                    citiesList.find(item => item.Key === newCityKey)?.LocalizedName ||
+                    citiesList.find(item => item.apiKey === newCityKey)?.name ||
                     'name is missing'
                 );
             }}
         >
-            {fromAPIListToOptionList(citiesList)};
+            {citiesToOptionList(citiesList)}
         </select>
     </>
 }
