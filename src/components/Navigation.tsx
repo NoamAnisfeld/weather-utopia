baa47e2161823c40fdadfe1807150349a9892cc8
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-09-11 10:27:54 +0300

    migratin navbar to bootstrap (experimental styling)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,21 @@
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Navigation() {
 
-    return <nav>
-        <Link to="/">Main</Link>
-        {" "}
-        <Link to="favorites">Favorites</Link>
-    </nav>
+    return <Navbar bg="secondary" variant="dark">
+        <Container fluid className="justify-content-end">
+            <Nav className="text-bg-primary">
+                <Nav.Item>
+                    <Link className="nav-link" to="/">Main</Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Link className="nav-link" to="favorites">Favorites</Link>
+                </Nav.Item>
+            </Nav>
+        </Container>
+    </Navbar>
 }
