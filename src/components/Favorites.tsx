d5bae04673fc435a87eb5d807119ad97949abe00
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-09-11 15:38:37 +0300

    layout: Favorites screen

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { FavoritesContext } from "../hooks/favoriteCitiesContext";
 import CityInfoWidget from "./CityInfoWidget";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Favorites() {
     const {
@@ -9,11 +12,15 @@ export default function Favorites() {
         removeFavoriteCity,
     } = useContext(FavoritesContext)
 
-    return <div className="favorites-area">
-        {favoriteCities.map(city => <CityInfoWidget {...{
-                cityKey: city.key,
-                cityName: city.name,
-            }} />
-        )}
-    </div>
+    return <Container className="favorites-area p-5">
+        <Row className="justify-content-center">
+            {favoriteCities.map(city => <Col className="col-auto">
+                <CityInfoWidget {...{
+                        cityKey: city.key,
+                        cityName: city.name,
+                    }} />
+                </Col>
+            )}
+        </Row>
+    </Container>
 }
